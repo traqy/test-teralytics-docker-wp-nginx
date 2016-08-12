246b0f2e96cd129dec3b2c9c6ae946fda6d4c129
traqy <bernard.t@mig.me>
2016-08-12 20:37:28 +0800

    Update backup.sh script.

--- backup-nfs-client/backup.sh
+++ backup-nfs-client/backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "Back up script is under construction".
-
-echo "Mount NFS 52.221.222.174:/exports/var/shareddir"
+#echo "Mount NFS 52.221.222.174:/exports/var/shareddir"
+echo "Backup started."
 mount -a
 
 export SOURCE_DIR=/var/www/upload
@@ -10,3 +9,4 @@ export NFS_BACKUP_DIR=/mnt/acme-su-nfs
 
 echo "rsync -az ${SOURCE_DIR} ${NFS_BACKUP_DIR}"
 rsync -rltzuv ${SOURCE_DIR} ${NFS_BACKUP_DIR}
+echo "Backup done."
